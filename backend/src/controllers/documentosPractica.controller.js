8c80cccc27c7575f3e778b2524af6b76f6ee80f7
Lucas23-IECI <lucas.mendez2201@alumnos.ubiobio.cl>
2024-10-29 00:19:28 -0300

    MOD Controller Multer

--- backend/src/controllers/documentosPractica.controller.js
+++ backend/src/controllers/documentosPractica.controller.js
@@ -7,10 +7,16 @@ import { handleErrorClient, handleErrorServer, handleSuccess } from "../handlers
 
 export async function subirDocumentoPractica(req, res) {
     try {
-        const { documento, fechaLimite } = req.body;
         const user = req.user;
 
-        const [nuevoDocumento, error] = await subirDocumentoService(user, documento, fechaLimite);
+        if (!req.file) {
+            return handleErrorClient(res, 400, "No se ha subido ningún archivo");
+        }
+
+        const { fechaLimite } = req.body;
+        const archivoPath = req.file.path;
+
+        const [nuevoDocumento, error] = await subirDocumentoService(user, archivoPath, fechaLimite);
 
         if (error) return handleErrorClient(res, 400, error);
 
