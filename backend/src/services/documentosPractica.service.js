ad5a1fb03b9e370df157b3f635c3d17c2bd9343f
Lucas23-IECI <lucas.mendez2201@alumnos.ubiobio.cl>
2024-10-29 01:35:29 -0300

    ADD Delete Service

--- backend/src/services/documentosPractica.service.js
+++ backend/src/services/documentosPractica.service.js
@@ -1,5 +1,5 @@
 "use strict";
-
+import fs from "fs";
 import { AppDataSource } from "../config/configDb.js";
 import DocumentosPractica from "../entity/documentosPractica.entity.js";
 
@@ -19,5 +19,26 @@ export async function subirDocumentoService(user, archivoPath, fechaLimite) {
 
     } catch (error) {
         return [null, "Error interno del servidor"];
+    }
+}
+
+export async function eliminarDocumentoService(user, documentoId) {
+    try {
+        const documentoRepository = AppDataSource.getRepository(DocumentosPractica);
+        const documento = await documentoRepository.findOne({
+            where: { id: documentoId, profesorId: user.id }
+        });
+        if (!documento) {
+            return [null, "Documento no encontrado o el usuario no tiene permisos para eliminarlo"];
+        }
+
+        fs.unlink(documento.documento,(err) => {
+            if (err) console.error("Error al eliminar el archivo:", err);
+        });
+        await documentoRepository.remove(documento);
+
+        return [true, null];
+    } catch (error) {
+        return [null, "Error al eliminar el documento"];
     }
 }
