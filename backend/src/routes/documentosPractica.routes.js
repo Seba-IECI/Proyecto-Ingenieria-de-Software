7900b4e332a295794427b25a8060a9b5f3707059
Lucas23-IECI <lucas.mendez2201@alumnos.ubiobio.cl>
2024-10-29 01:31:44 -0300

    ADD Delete

--- backend/src/routes/documentosPractica.routes.js
+++ backend/src/routes/documentosPractica.routes.js
@@ -1,12 +1,13 @@
 "use strict";
 import { Router } from "express";
 import { authenticateJwt } from "../middlewares/authentication.middleware.js";
-import { subirDocumentoPractica } from "../controllers/documentosPractica.controller.js";
+import { eliminarDocumentoPractica, subirDocumentoPractica  } from "../controllers/documentosPractica.controller.js";
 import { handleFileSizeLimit, upload } from "../middlewares/subirArchivos.middleware.js";
 
 const router = Router();
 
 router
     .post("/subir", authenticateJwt, upload.single("archivo"), handleFileSizeLimit, subirDocumentoPractica)
+    .delete("/eliminar/:id", authenticateJwt, eliminarDocumentoPractica);
 
 export default router;
