3995de9017475565fbd32ae63ee50289d71655a0
Lucas23-IECI <lucas.mendez2201@alumnos.ubiobio.cl>
2024-10-29 01:32:56 -0300

    ADD Delete practica

--- backend/src/controllers/documentosPractica.controller.js
+++ backend/src/controllers/documentosPractica.controller.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { subirDocumentoService } from "../services/documentosPractica.service.js";
+import { eliminarDocumentoService, subirDocumentoService } from "../services/documentosPractica.service.js";
 
 import { handleErrorClient, handleErrorServer, handleSuccess } from "../handlers/responseHandlers.js";
 
@@ -23,5 +23,18 @@ export async function subirDocumentoPractica(req, res) {
         handleSuccess(res, 201, "Documento subido correctamente.", nuevoDocumento);
     } catch (error) {
         handleErrorServer(res, 500, "Error subiendo el documento.");
+    }
+}
+
+export async function eliminarDocumentoPractica(req, res) {
+    try {
+        const documentoId = req.params.id;
+        const user = req.user;
+        const [res, error] = await eliminarDocumentoService(user, documentoId);
+
+        if (error) return handleErrorClient(res, 400, error);
+        handleSuccess(resultado, 200, "Documento eliminado correctamente.");
+    } catch (error) {
+        handleErrorServer(res, 500, "Error eliminando el documento.");
     }
 }
