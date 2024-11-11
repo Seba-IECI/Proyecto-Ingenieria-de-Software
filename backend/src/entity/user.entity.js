d9bc3e3e02c72a1315f4e9b2bf37b9d09263a224
Handrades <humberto.andrades2201@alumnos.ubiobio.cl>
2024-11-10 21:10:50 -0300

    añadi campo permisos y restableci nivel en entity.user

--- backend/src/entity/user.entity.js
+++ backend/src/entity/user.entity.js
@@ -32,6 +32,11 @@ const UserSchema = new EntitySchema({
       length: 50,
       nullable: false,
     },
+    nivel: {
+      type: "varchar",
+      length: 3,
+      nullable: true,
+    },
     permisos: {
       type: "varchar", 
       length: 50,
