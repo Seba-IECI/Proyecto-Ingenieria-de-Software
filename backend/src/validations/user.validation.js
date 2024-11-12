a74d931e48dfcf32921c6248ecd903f7da9fbe7e
Lucas23-IECI <lucas.mendez2201@alumnos.ubiobio.cl>
2024-11-11 21:10:24 -0300

    ADD Cambios en el user, reestablecer nivel

--- backend/src/validations/user.validation.js
+++ backend/src/validations/user.validation.js
@@ -33,7 +33,7 @@ export const userQueryValidation = Joi.object({
         "El correo electrónico debe tener como máximo 35 caracteres.",
     })
     .custom(domainEmailValidator, "Validación dominio email"),
-    rut: Joi.string()
+  rut: Joi.string()
     .min(9)
     .max(12)
     .pattern(/^(?:(?:[1-9]\d{0}|[1-2]\d{1})(\.\d{3}){2}|[1-9]\d{6}|[1-2]\d{7}|29\.999\.999|29999999)-[\dkK]$/)
