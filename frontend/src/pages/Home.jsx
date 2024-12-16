e39e55129417ef3a2627793d910203a3b9aaf633
Handrades <humberto.andrades2201@alumnos.ubiobio.cl>
2024-12-15 22:57:30 -0300

    ADD imagen al Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,31 @@
+import WebImage from "../assets/Web-LN-23.png";
+
 const Home = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div style={styles.container}>
+      <img 
+        src={WebImage} 
+        alt="Descripción de la imagen" 
+        style={styles.image}
+      />
+    </div>
+  );
+};
 
-export default Home
+const styles = {
+  container: {
+    backgroundColor: "#f5f5f5",
+    display: "flex", 
+    justifyContent: "center", 
+    alignItems: "center", 
+    height: "100vh", 
+    margin: 0, 
+  },
+  image: {
+    width: "1000px",   
+    height: "auto",   
+    objectFit: "contain", 
+  }
+};
+
+export default Home;
